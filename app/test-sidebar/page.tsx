743c1cb1c2d5b2ba858bb3a13ba026d3553ae51d
JCBEA <111287289+JCBEA@users.noreply.github.com>
2025-05-16 20:37:37 +0800

    asdasd

---
+++ app/test-sidebar/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import MainSidebar from '@/components/main-sidebar'
+
+export default function TestSidebarPage() {
+  return (
+    <MainSidebar>
+      <div className="p-4">
+        <h1>Test Sidebar Page</h1>
+        <p>This is a minimal page to test the sidebar rendering.</p>
+      </div>
+    </MainSidebar>
+  )
+}
